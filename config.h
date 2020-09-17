96dfbf2708e822dfd4eea79d99af55841de27eab
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-09-17 14:00:50 -0300

    Move fallback colors to separate file

--- config.h
+++ config.h
@@ -10,14 +10,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 /* Colorscheme from pywal */
 #include "/home/rafael/.cache/wal/colors-wal-dmenu.h"
-/* Use this if pywal unavailable
-static const char *colors[SchemeLast][2] = {
-       \/*     fg         bg       *\/
-       [SchemeNorm] = { "#bbbbbb", "#222222" },
-       [SchemeSel] = { "#eeeeee", "#005577" },
-       [SchemeOut] = { "#000000", "#00ffff" },
-};
-*/
+/* use this if pywal unavailable */
+/*#include "colors-default.h"*/
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
