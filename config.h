78f99fec12e9696b1065795cb71c18cbc6d0bc2d
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-02-28 03:33:36 -0300

    Apply "lineheight" patch

--- config.h
+++ config.h
@@ -15,8 +15,13 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static unsigned int lines      = 0;
 static unsigned int columns    = 0;
 
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 20;
+static unsigned int min_lineheight = 20;
+
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
