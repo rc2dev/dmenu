0010a427b130b7102ef26bd7d108827a896f9a98
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-07-22 17:44:39 -0300

    Make movements more vim-like
    Before, the only way to move between consecutive items was using alt+hl.
    
    This makes the movements as you expect: hl for horizontal, jk for
    vertical. Besides that, on horizontal lists, jk also works.
    
    Specifically:
    
    - Vertical lists: Move with alt+jk instead of alt+hl.
    - Horizontal lists (extra): Move with alt+jk besides alt+hl.
    - Grids: alt+hl moves horizontally instead of vertically.

--- dmenu.c
+++ dmenu.c
@@ -447,10 +447,18 @@ keypress(XKeyEvent *ev)
 			goto draw;
 		case XK_g: ksym = XK_Home;  break;
 		case XK_G: ksym = XK_End;   break;
-		case XK_h: ksym = XK_Up;    break;
-		case XK_j: ksym = XK_Next;  break;
-		case XK_k: ksym = XK_Prior; break;
-		case XK_l: ksym = XK_Down;  break;
+		case XK_j:
+			ksym = XK_Down;
+			break;
+		case XK_k:
+			ksym = XK_Up;
+			break;
+		case XK_h:
+			columns > 0 ? ksym = XK_Left : (ksym = XK_Up);
+			break;
+		case XK_l:
+			columns > 0 ? ksym = XK_Right : (ksym = XK_Down);
+			break;
 		default:
 			return;
 		}
