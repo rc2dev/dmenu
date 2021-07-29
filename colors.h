47ce4c19a649cb530e5d90f8dfd3399b91cc4ca5
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-07-29 19:31:46 -0300

    Use Nord colors

--- colors.h
+++ colors.h
@@ -1,8 +1,8 @@
 static const char *colors[SchemeLast][2] = {
        /*     fg         bg       */
-       [SchemeNorm] = { "#bbbbbb", "#222222" },
-       [SchemeSel] = { "#eeeeee", "#005577" },
-       [SchemeSelHighlight] = { "#ffc978", "#005577" },
-       [SchemeNormHighlight] = { "#ffc978", "#222222" },
-       [SchemeOut] = { "#000000", "#00ffff" },
+       [SchemeNorm] = { "#81a1c1", "#2e3440" },
+       [SchemeSel] = { "#e5e9f0", "#4c566a" },
+       [SchemeSelHighlight] = { "#ffc978", "#4c566a" },
+       [SchemeNormHighlight] = { "#ffc978", "#2e3440" },
+       [SchemeOut] = { "#2e3440", "#00ffff" },
 };
